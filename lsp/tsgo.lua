2a95ef90b84a48a3ada49651ea47a3b8036f1042
Anthony Mittaz <anthony.mittaz@zip.co>
2026-07-09 14:50:11 +1000

    Change command from tsgo to tsc now that 7 is out

--- lsp/tsgo.lua
+++ lsp/tsgo.lua
@@ -61,16 +61,7 @@ return {
       },
     },
   },
-  cmd = function(dispatchers, config)
-    local cmd = 'tsgo'
-    if (config or {}).root_dir then
-      local local_cmd = vim.fs.joinpath(config.root_dir, 'node_modules/.bin', cmd)
-      if vim.fn.executable(local_cmd) == 1 then
-        cmd = local_cmd
-      end
-    end
-    return vim.lsp.rpc.start({ cmd, '--lsp', '--stdio' }, dispatchers)
-  end,
+  cmd = { 'tsc', '--lsp', '--stdio' },
   filetypes = {
     'javascript',
     'javascriptreact',
